84eca1d9f696659ee5d94f2214aee88017ae98eb
Carsten <df3xc@web.de>
2018-11-03 03:40:33 +0100

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-
 echo '-------------------------------------------------------------------'
 echo ' Remove packages from BeagleBone not needed for headless operation'
 echo '-------------------------------------------------------------------'
+apt-get remove connman
+apt-get remove xserver*
+apt-get autoremove
+apt-get remove x11*
+apt-get remove libqt*
+apt-get autoremove
+apt-get remove xfo*
+apt-get autoremove
+apt-get remove xorg*
+apt-get remove ruby*
+apt-get autoremove
+echo ' ------------- done --------------'
