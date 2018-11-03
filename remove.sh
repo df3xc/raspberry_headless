b7e5b61b0e5bc0cc933d56c54401b331abccc236
Carsten <df3xc@web.de>
2018-11-03 03:37:23 +0100

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo '-------------------------------------------------------------------'
+echo ' Remove packages from BeagleBone not needed for headless operation'
+echo '-------------------------------------------------------------------'
