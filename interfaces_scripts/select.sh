512f1ce9ef9c19d3ce65797033f4727313555d2d
Carsten <df3xc@web.de>
2018-11-03 14:35:20 +0100

    Add files via upload

---
+++ interfaces_scripts/select.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+echo " select interfaces : " $1
+
+if [ $1 = 'static' ]
+then
+   ifconfig wlan0 down
+   cp interfaces.static interfaces
+   echo "nameserver 192.168.2.1" >/etc/resolv.conf
+   ifup wlan0
+   route add default gw 192.168.2.1 wlan0
+   echo " done " $1
+fi
+
+if [ $1 = 'ics' ]
+then
+   ifconfig wlan0 down
+   cp interfaces.ics interfaces
+   echo "nameserver 192.168.137.1" >/etc/resolv.conf
+   ifup wlan0
+   route add default gw 192.168.137.1 wlan0
+   echo " done " $1
+fi
+
+if [ $1 = 'iphone' ]
+then
+   ifconfig wlan0 down
+   cp interfaces.iphone interfaces
+   echo "nameserver 172.20.10.1" >/etc/resolv.conf
+   ifup wlan0
+   route add default gw 172.20.10.1 wlan0
+   echo " done " $1
+fi
